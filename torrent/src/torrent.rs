2bb6b6a2a7dc35eb6e40ce9e67ac517b89d7962f
CroxxN <saradgaihre19@gmail.com>
2024-10-17 13:10:27 +0545

    impl: deserialization

--- torrent/src/torrent.rs
+++ torrent/src/torrent.rs
@@ -2,6 +2,7 @@ use core::panic;
 
 use bencode;
 use bencode::bencode::BTypes;
+use error::TorrentError;
 
 pub struct Torrent {
     pub announce: String,
@@ -31,16 +32,41 @@ pub struct Files {
 }
 
 impl Torrent {
-    pub fn from_str(val: &str) {
+    pub fn from_str(val: &str) -> Result<Self, TorrentError> {
+        let mut torrent = Self::default();
         let mut u8s = bencode::utils::bcode_to_u8(val);
-        let mut announce: String;
         while let Ok(b) = bencode::bencode::decode(&mut u8s) {
             match b {
-                BTypes::DICT(d) => d.into_iter().for_each(|(k, v)| match k.as_str() {
-                    "announce" => announce = v.try_into(),
-                }),
-                _ => panic!("NOT A TORRENT FILE"),
+                BTypes::DICT(d) => d.into_iter().for_each(|(k, v)| self.de_fields(k, v)?),
+                _ => Err(TorrentError::UnexpectedField)
             }
         }
+        Ok(torrent)
+    }
+
+    fn de_fields(&mut self, k: String, v: BTypes)-> Result<(), DecodeError> {
+        match k.to_str() {
+            "announce" => self.announce = v.try_into()?,
+            "announce-list" => self.announce_list = v.try_into()?,
+            "creation date" => self.creation_date = v.try_into()?,
+            "comment" => self.comment = v.try_into()?,
+            "created by" => self.created_by = v.try_into()?,
+            "encoding" => self.encoding = v.try_into()?,
+            "info" => {
+                if let BTypes::DICT(d) = v {
+                    d.into_inter().for_each(|(k, v)| self.de_info_fields(k, v));
+                } else {
+                    Err(TorrentError::UnexpectedField);
+                }
+            },
+            _ => Err(TorrentError::UnexpectedField)
+        }
+    }
+
+    fn de_info_fields(&mut self, k: String, v: BTypes) -> Result<(), DecodeError>{
+        match k.to_str() {
+            "name" => self.info.name = v.try_into()?,
+        }
+            
     }
 }
